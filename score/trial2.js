8cdedc0faea983515296dffa4cb231211e6d7520
graceibam <grace.bambushew@tufts.edu>
2013-04-14 21:08:30 -0400

    experimenting with trial

---
+++ score/trial2.js
@@ -0,0 +1,9 @@
+
+var mongoUri = 'mongodb://gbambushew:murray@dharma.mongohq.com:10038/scorecenter'
+var mongo = require('mongodb');
+var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
+	db = databaseConnection;
+	db.collection('highscores', function(er, collection) {
+			collection.insert({'soda':'moxie'},{w:1}, function(err, result){});
+	});
+});

a8624c131a76985978820d9435a8cb6f366fb40e
graceibam <grace.bambushew@tufts.edu>
2013-04-15 23:21:56 -0400

    score num

--- score/testScores.js
+++ score/testScores.js
@@ -3,7 +3,7 @@
 	 $("#subScores").click(function(){
 		
 		var nombre=$("#username").val();
-		var score=100//$("#score").val();
+		var score=20//$("#score").val();
 		console.log(nombre);
 		console.log(score);
 		$("#username").val('');
